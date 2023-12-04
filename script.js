c6d510dd20a849015a5e3489882c4733db241fcf
Wysmith00 <wysmith00@gmail.com>
2023-12-04 11:54:27 -0500

    updated card deck with shuffle

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const deck = [
-    "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
-    "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
-    "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
-    "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
-];
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let suits = ["Diamonds", "Spades", "Hearts", "Clubs"];
+// adding the card deck and shuffle feature //
+funciton makeDeck()
+    let deck = [];
+
+    for (let suit of suits) {
+        for (let value of values) {
+            deck.push('${value}' of '${suit}');
+        }
+    }
+
+    return deck;
+}
+
+let cardDeck = makeDeck();
+// makeDeck iuses loops to iterate each combination in the array to cover all 52 possible combinations to create the cardDeck array//
+ 
